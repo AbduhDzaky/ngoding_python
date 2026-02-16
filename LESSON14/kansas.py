a7dd018d706a1eb04dbd4936f1a0d95b64d93f4e
Abduhdzaky <abduhdzaky000@gmail.com>
2026-02-16 17:29:24 +0700

    lesson 13-15

---
+++ LESSON14/kansas.py
@@ -0,0 +1,24 @@
+from random import choice
+
+capital = "Topeka"
+
+bird = "Western Meadowlark"
+
+flower = "Peony"
+
+song = "That Somebody"
+
+def randomfunfact():
+    funfacts = [
+        "Kansas is considered flat, but it does have a mountain.",
+        "Wichita is the largest city in the state, but many would guess that it is Kansas City.",
+        "A considerable portion of Kansas City is actually in Missouri.",
+        "Most Kansans are annoyed by wizzard of Oz references from people outside of Kansas"
+    ]
+
+    index = choice("0123")
+
+    print(funfacts[int(index)])
+
+if __name__ == "__main__":
+    randomfunfact()
