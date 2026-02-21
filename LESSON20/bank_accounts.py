051f0524a0c15e7531e424e673e5c0d185b34bf4
Abduhdzaky <abduhdzaky000@gmail.com>
2026-02-21 15:34:41 +0700

    Lesson 20

---
+++ LESSON20/bank_accounts.py
@@ -0,0 +1,63 @@
+class BalanceException(Exception):
+    pass
+
+class BankAccount:
+    def __init__(self, initial_amount, acct_name):
+        self.balance = initial_amount
+        self.name = acct_name
+        print(f"\nAccount '{self.name}' created.\nBalance = ${self.balance:.2f}")
+
+    def get_balance(self): # Cek Saldo
+        print(f"\nAccount '{self.name}' balance = ${self.balance:2f}")
+
+    def deposit(self, amount): # Menabung (setor tunai)
+        self.balance = self.balance + amount
+        print("\nDeposit complete.")
+        self.get_balance()
+
+    def viable_transaction(self, amount): #satpam penjaga saldo
+        if self.balance >= amount:
+            return
+        else:
+            raise BalanceException(
+                f"\nSorry, account '{self.name}' only has a balance of ${self.balance:.2f}"
+            )
+    
+    def withdraw(self, amount):
+        try:
+            self.viable_transaction(amount)
+            self.balance = self.balance - amount
+            print("\nWithdraw complete.")
+            self.get_balance()
+        except BalanceException as error:
+            print(f"\nWithdraw interrupted: {error}")
+
+    def transfer(self, amount, account):
+        try:
+            print('\n********\nBeginning Transfer.. 🚀')
+            self.viable_transaction
+            self.withdraw
+            account.deposit(amount)
+            print('\nTransfer complete! ✅\n\n**********')
+        except BalanceException as error:
+            print(f'\nTransfer interrupted. ❌ {error}')
+
+class InterestRewardsAcct(BankAccount):
+    def deposit(self, amount):
+        self.balance = self.balance + (amount * 1.05)
+        print("\nDeposit complete.")
+        self.get_balance()
+
+class SavingsAcct(InterestRewardsAcct):
+    def __init__(self, initial_amount, acct_name):
+        super().__init__(initial_amount, acct_name)
+        self.fee = 5
+
+    def withdraw(self, amount):
+        try:
+            self.viable_transaction(amount + self.fee)
+            self.balance = self.balance - (amount + self.fee)
+            print("\nWithdraw complete.")
+            self.get_balance()
+        except BalanceException as error:
+            print(f"\nWithdraw interrupted: {error}")
